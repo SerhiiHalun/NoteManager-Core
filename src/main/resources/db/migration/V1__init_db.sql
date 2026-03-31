a016053ff9c8323db77d0c961cc555b82db906f8
SerhiiHalun <sergey.galun15@gmail.com>
2026-03-31 22:11:41 +0200

    Reworked DB migrate file:  -V1__ delete insert sql code  -V4__ alter note column on TEXT

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,8 +1,5 @@
 CREATE TABLE note (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      id BIGSERIAL PRIMARY KEY,
                       title VARCHAR(300) NOT NULL,
                       content VARCHAR(1000)
 );
-insert into note( title, content) values
-        ('First Note','This is the content of the first note.'),
-        ('Second Note','This is the content of the second note.')
